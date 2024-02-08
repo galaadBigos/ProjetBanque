d2f73f7f1a97ef9103324b8abaf235c5b28a0119
lwalter57 <ludovic.walter@stagiairesmns.fr>
2024-02-08 15:53:09 +0100

    Implémentation classe entite

--- src/ProjetBanque/Models/Clients/Client.cs
+++ src/ProjetBanque/Models/Clients/Client.cs
@@ -3,12 +3,17 @@ using ProjetBanque.Models.Comptes;
 
 namespace ProjetBanque.Models.Clients
 {
-	public class Client(string numeroClient, string adresse, string nom)
+	public class Client(string numeroClient, string adresse, string nom) : Entite
 	{
 		public string NumeroClient { get; set; } = numeroClient;
 		public string Adresse { get; set; } = adresse;
 		public string Nom { get; set; } = nom;
 		public Banque Banque { get; set; }
 		public List<Compte> Comptes { get; set; }
-	}
+
+        public override Entite ConvertirEnDTO()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
