38cc770e99c3c2b29845cb80a8bab8014e12391c
Brice Briçou <46904213+Brice5423@users.noreply.github.com>
2024-02-12 11:15:40 +0100

    🟧+~ CompteTests: Crediter (~1 fois et +2 fois)

--- tests/ProjetBanque.UnitTests/Models/Comptes/CompteTests.cs
+++ tests/ProjetBanque.UnitTests/Models/Comptes/CompteTests.cs
@@ -12,7 +12,7 @@ public class CompteTests
     [TestMethod]
     [DataRow(50.0d)]
     [DataRow(250.0d)]
-    public void Crediter_SoldeInitial0_RetourneSoldeMisAJour(double montant)
+    public void Crediter_SoldeInitial0EtCrediterUneFois_RetourneSoldeMisAJour(double montant)
     {
         double soldeOrigine = _compteMock.Object.Solde;
 
@@ -21,6 +21,22 @@ public class CompteTests
 
         // Assert
         nouvelleSolde.Should().Be(soldeOrigine + montant);
+        _compteMock.Object.Solde.Should().Be(nouvelleSolde);
+    }
+
+    [TestMethod]
+    [DataRow(50.0d, 25.0d)]
+    [DataRow(250.0d, 99.99d)]
+    public void Crediter_SoldeInitial0EtCrediterDeuxFois_RetourneSoldeMisAJour(double montant1, double montant2)
+    {
+        double soldeOrigine = _compteMock.Object.Solde;
+
+        // Act
+        _compteMock.Object.Crediter(montant1);
+        double nouvelleSolde = _compteMock.Object.Crediter(montant2);
+
+        // Assert
+        nouvelleSolde.Should().Be(soldeOrigine + montant1 + montant2);
         _compteMock.Object.Solde.Should().Be(nouvelleSolde);
     }
 }
