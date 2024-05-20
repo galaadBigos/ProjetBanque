ca7c02ebf3429c5732ef3cfdf2304f83a2e2261b
Brice <briceorl54580@gmail.com>
2024-05-20 21:06:04 +0200

    🟧~ BanqueTests.cs: Fix, amélioration et format

--- tests/UnitTests/ProjetBanque.UnitTests/Models/Banques/BanqueTests.cs
+++ tests/UnitTests/ProjetBanque.UnitTests/Models/Banques/BanqueTests.cs
@@ -1,131 +1,125 @@
 using AutoFixture;
 using FluentAssertions;
 using Moq;
-using ProjetBanque.Abstractions;
 using ProjetBanque.Abstractions.Models;
 using ProjetBanque.Models.Banques;
 
-namespace ProjetBanque.UnitTests.Models.Banques
+namespace ProjetBanque.UnitTests.Models.Banques;
+
+[TestClass]
+public class BanqueTests
 {
-	[TestClass]
-	public class BanqueTests
-	{
-		private readonly Fixture _fixture = new();
-		private readonly Banque _banque = new();
-		private readonly Mock<IClient> _clientMock = new();
-		private readonly Mock<ICompte> _compteMock = new();
+    private readonly int _soldeInitial = 1000;
+    private readonly Fixture _fixture = new();
+    private readonly Banque _banque = new();
+    private readonly Mock<IClient> _clientMock = new();
+    private readonly Mock<ICompte> _compteMock = new();
 
-		[TestInitialize]
-		public void TestInitialize()
-		{
-			InitialisationClientMock();
-			InitialisationCompteMock();
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        InitialisationCompteMock();
+        InitialisationClientMock();
 
-			_clientMock.Setup(c => c.Comptes).Returns([_compteMock.Object]);
-			_banque.Clients.Add(_clientMock.Object);
-		}
+        _banque.Nom = "MaBanque";
+        _banque.Comptes.Add(_compteMock.Object);
+        _banque.Clients.Add(_clientMock.Object);
+    }
 
-		private void InitialisationClientMock()
-		{
-			_clientMock.Setup(c => c.NumeroClient).Returns(_fixture.Create<string>());
-			_clientMock.Setup(c => c.Adresse).Returns(_fixture.Create<string>());
-			_clientMock.Setup(c => c.Nom).Returns(_fixture.Create<string>());
-		}
+    private void InitialisationCompteMock()
+    {
+        _compteMock.Setup(c => c.NumeroCompte).Returns(_fixture.Create<string>());
+        _compteMock.Setup(c => c.Solde).Returns(_soldeInitial);
+    }
 
-		private void InitialisationCompteMock()
-		{
-			_compteMock.Setup(c => c.NumeroCompte).Returns(_fixture.Create<string>());
-		}
+    private void InitialisationClientMock()
+    {
+        _clientMock.Setup(c => c.NumeroClient).Returns(_fixture.Create<string>());
+        _clientMock.Setup(c => c.Adresse).Returns(_fixture.Create<string>());
+        _clientMock.Setup(c => c.Nom).Returns(_fixture.Create<string>());
+        _clientMock.Setup(c => c.Comptes).Returns([_compteMock.Object]);
+    }
 
-		[TestMethod]
-		public void ConstructeurVide_ClientsEtComptesSontListesVides()
-		{
-			// Arrange
-			Banque banque = new Banque();
+    [TestMethod]
+    public void ConstructeurVide_ClientsEtComptesSontListesVides()
+    {
+        Banque banque = new Banque();
 
-			// Act
-			List<IClient> clients = banque.Clients;
-			List<ICompte> comptes = banque.Comptes;
+        banque.Nom.Should().BeEmpty();
+        banque.Clients.Should().HaveCount(0);
+        banque.Comptes.Should().HaveCount(0);
+    }
 
-			// Assert
-			clients.Should().HaveCount(0);
-			comptes.Should().HaveCount(0);
-		}
+    [TestMethod]
+    [DataRow(500d)]
+    [DataRow(8000d)]
+    public void Depot_DeposeXArgentSurUnCompte_RetourneSoldePlusX(double montant)
+    {
+        string clientNom = _clientMock.Object.Nom;
+        string compteNumero = _compteMock.Object.NumeroCompte;
 
-		[TestMethod]
-		[DataRow(500d)]
-		[DataRow(8000d)]
-		public void Retrait_VerificationSiRetraitAppelDebiter_DebiterEffectue(double montant)
-		{
-			// Arrange
-			string compteNumero = _compteMock.Object.NumeroCompte;
-			string clientNom = _clientMock.Object.Nom;
+        _compteMock.Setup(c => c.Crediter(montant)).Returns(_soldeInitial + montant);
 
-			// Act
-			_banque.Retrait(compteNumero, clientNom, montant);
+        double? nouveauSolde = _banque.Depot(compteNumero, clientNom, montant);
 
-			// Assert
-			_compteMock.Verify(c => c.Debiter(montant), Times.Once);
-		}
+        _compteMock.Verify(c => c.Crediter(montant), Times.Once);
+        nouveauSolde.Should().Be(_soldeInitial + montant);
+    }
 
-		[TestMethod]
-		[DataRow(500d)]
-		[DataRow(8000d)]
-		public void Depot_DeposeXArgentSurUnCompte_RetourneSoldePlusX(double montant)
-		{
-            // Arrange
-            string clientNom = _clientMock.Object.Nom;
-            string compteNumero = _clientMock.Object.NumeroClient;
+    [TestMethod]
+    [DataRow(500d)]
+    [DataRow(8000d)]
+    public void Depot_DemandeDepotSurCompteInexistant_RetourneNull(double montant)
+    {
+        string compteNumero = _compteMock.Object.NumeroCompte;
+        string clientNom = _clientMock.Object.Nom;
 
+        string compteNumeroInexistant;
+        do
+        {
+            compteNumeroInexistant = _fixture.Create<string>();
+        } while (compteNumeroInexistant == compteNumero);
 
-            // Act
-            _banque.Depot(compteNumero, clientNom, montant);
+        double? montantDeposer = _banque.Depot(compteNumeroInexistant, clientNom, montant);
 
-			// Assert
-			_compteMock.Verify(c => c.Crediter(montant), Times.Once);
+        _compteMock.Verify(c => c.Crediter(montant), Times.Never);
+        montantDeposer.Should().BeNull();
+    }
 
-		}
+    [TestMethod]
+    [DataRow(500d)]
+    [DataRow(8000d)]
+    public void Depot_DemandeDepotSurClientInexistant_RetourneNull(double montant)
+    {
+        string compteNumero = _compteMock.Object.NumeroCompte;
+        string clientNom = _clientMock.Object.Nom;
 
-		[TestMethod]
-		public void Depot_DemandeDepotSurCompteInexistant_RetourneNull()
-		{
-			// Arrange
-			string clientNom = _clientMock.Object.Nom;
-			string compteNumero = _clientMock.Object.NumeroClient;
+        string clientNomInexistant;
 
-			string compteNumeroInexistant = "789";
-			int montant = 500;
+        do
+        {
+            clientNomInexistant = _fixture.Create<string>();
+        } while (clientNomInexistant == clientNom);
 
-			// Act
-			double? montantDeposer = _banque.Depot(compteNumeroInexistant, clientNom, montant);
+        double? montantDeposer = _banque.Depot(compteNumero, clientNomInexistant, montant);
 
-			// Assert
-			_compteMock.Verify(c => c.Crediter(montant), Times.Never);
-			montantDeposer.Should().BeNull();
-		}
+        _compteMock.Verify(c => c.Crediter(montant), Times.Never);
+        montantDeposer.Should().BeNull();
+    }
 
-		[TestMethod]
-		[DataRow(500d)]
-		[DataRow(8000d)]
-		public void Depot_DemandeDepotSurClientInexistant_RetourneNull(double montant)
-		{
-			// Arrange
-			string compteNumero = _compteMock.Object.NumeroCompte;
-			string clientNom = _clientMock.Object.Nom;
+    [TestMethod]
+    [DataRow(500d)]
+    [DataRow(8000d)]
+    public void Retrait_VerificationSiRetraitAppelDebiter_DebiterEffectue(double montant)
+    {
+        string clientNom = _clientMock.Object.Nom;
+        string compteNumero = _compteMock.Object.NumeroCompte;
 
-			string clientNomInexistant;
+        _compteMock.Setup(c => c.Debiter(montant)).Returns(_soldeInitial - montant);
 
-			do
-			{
-				clientNomInexistant = _fixture.Create<string>();
-			} while (clientNomInexistant == clientNom);
+        double? nouveauSolde = _banque.Retrait(compteNumero, clientNom, montant);
 
-			// Act
-			double? montantDeposer = _banque.Depot(compteNumero, clientNomInexistant, montant);
-
-			// Assert
-			_compteMock.Verify(c => c.Crediter(montant), Times.Never);
-			montantDeposer.Should().BeNull();
-		}
-	}
+        _compteMock.Verify(c => c.Debiter(montant), Times.Once);
+        nouveauSolde.Should().Be(_soldeInitial - montant);
+    }
 }
